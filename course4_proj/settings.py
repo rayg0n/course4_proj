daee21289ad8889d9b2d8a9abc234bb548ef541a
Anup Adhikari <aanupiw@gmail.com>
2024-06-09 16:00:56 +0000

    scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -47,7 +47,8 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
-        'django_celery_results'
+        'django_celery_results',
+        'django_celery_beat'
     ]
 
     MIDDLEWARE = [
