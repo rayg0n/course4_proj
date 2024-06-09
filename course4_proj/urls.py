2461f499a0cebb136c843e644e2e3dc0f0e2cc0a
Anup Adhikari <aanupiw@gmail.com>
2024-06-09 12:13:16 +0000

    github

--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import gh.views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", gh.views.index)
+
 ]
